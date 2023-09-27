21a782d59beedfb80addbba44e150237781a49c5
Екатерина Федулова <fedulova103@gmail.com>
2023-09-27 17:09:38 +0500

    Добавлена шестая практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int[] massive = new int[20];
+            for (int i = -15; i <= 14; i++)
+            {
+                massive[count] = i;               
+            }
+            Array.Sort(massive);
+            int maxNum = massive[0];
+            int maxAbsCount = 0;
+            for(int i1 = 0; i1 < 20; i1++)
+            {
+                if (Math.Abs(massive[i1]) > maxNum)
+                {
+                    maxAbsCount += 1;
+                }
+            }
+            Console.WriteLine($"Количество элементов по модулю больших, чем максимальный: {maxAbsCount}");
+
+
+        }
+    }
+}
