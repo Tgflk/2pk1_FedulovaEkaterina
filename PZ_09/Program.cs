fc4cd4874c15a8be928a40b44ebdd8c269f27de8
Федулова Екатерина <fedulova_katena@mail.ru>
2023-11-19 13:14:03 +0500

    Добавлена 9 практика

---
+++ PZ_09/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите путь к файлу на диске:");
+            string filePath = Console.ReadLine();
+
+            // Проверяем корректность пути
+            if (!filePath.StartsWith("D:\\") && !filePath.StartsWith("C:\\"))
+            {
+                Console.WriteLine("Некорректный путь. Путь должен начинаться с D:\\ или C:\\");
+                return;
+            }
+
+            // Разделяем путь на каталоги
+            string[] directories = filePath.Split('\\');
+
+            // Выводим названия каталогов
+            Console.WriteLine("Названия каталогов в пути:");
+            for (int i = 0; i < directories.Length - 1; i++) // Игнорируем имя файла, поэтому длина массива минус 1
+            {
+                Console.WriteLine(directories[i]);
+            }
+        }
+    }
+}
