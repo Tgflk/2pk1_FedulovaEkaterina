1119671638e9eb6961d2fc31d576f47294dba40d
Федулова Екатерина <fedulova_katena@mail.ru>
2023-11-24 18:07:10 +0500

    добавлена новая практика

---
+++ PZ_10/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PZ_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = 0;
+            char letter = ' ';
+
+            for (char c = 'a'; c < 'z'; c++)
+            {
+                int tmp = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == c)
+                        tmp++;
+                }
+                if (tmp > count)
+                {
+                    count = tmp;
+                    letter = c;
+                }
+            }
+
+            Console.WriteLine(letter + " Встречается " + count + " раз(-а)");
+            Console.ReadKey();
+        }
+    }
+}
